89bf31dbfd6d0d3396bfa98e38c9a834fc420244
Henry Baughman <hbaughman741@gmail.com>
2016-01-18 10:51:16 -0500

    Dropbox#revisions get revisions for todo file

--- dropbox.js
+++ dropbox.js
@@ -11,7 +11,7 @@ Dropbox.prototype.defaultHeaders = {
   "Content-Type": "application/json"
 };
 
-Dropbox.prototype.downloadTodo = function(callback, path) {
+Dropbox.prototype.downloadTodo = function(callback) {
   request({
     url: "https://content.dropboxapi.com/2/files/download",
     headers: {
@@ -23,6 +23,23 @@ Dropbox.prototype.downloadTodo = function(callback, path) {
   }, callback)
 };
 
+Dropbox.prototype.revisions = function(callback) {
+  request.post({
+    url: "https://api.dropboxapi.com/2/files/list_revisions",
+    //headers: this.defaultHeaders,
+    headers: {
+      Authorization: "Bearer " + this.accessToken,
+      "Content-Type": "application/json"
+    },
+    json: {
+      "path": this.todoPath,
+      // We need all the revisions since the last last check/update. We'll need
+      // to repoll if this is insufficient.
+      "limit": 10
+    }
+  }, callback)
+}
+
 function logResponse(err, resp, body) {
   if (err) throw err;
   console.log(resp.statusCode, body);
@@ -31,4 +48,4 @@ function logResponse(err, resp, body) {
 module.exports = Dropbox;
 
 var dropbox = new Dropbox("/todo/todo.txt", options.access_token)
-dropbox.downloadTodo(logResponse);
+dropbox.revisions(logResponse);
