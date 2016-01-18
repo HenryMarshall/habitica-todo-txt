89e3b96a7e7ddd5ce2a171536b7508020ff00dd3
Henry Baughman <hbaughman741@gmail.com>
2016-01-18 08:19:00 -0500

    Filter todos from Habitica tasks

--- habitica.js
+++ habitica.js
@@ -10,17 +10,32 @@ function Habitica(credentials) {
 
 Habitica.prototype.baseUrl = "https://habitica.com/api/v2";
 
-Habitica.prototype.tasks = function(callback, task) {
-  if (task === undefined) {
-    request({
-      url: this.baseUrl + "/user/tasks",
-      headers: this.headers
-    }, callback);
-  }
+// `tasks` also includes "rewards"
+Habitica.prototype.tasks = function(callback) {
+  request({
+    url: this.baseUrl + "/user/tasks",
+    headers: this.headers
+  }, callback);
 }
 
+Habitica.prototype.todos = function(callback) {
+  this.tasks(function(err, resp, body) {
+    if (err) throw err;
+    var tasks = JSON.parse(body);
+    var todos = tasks.filter(function(task) {
+      return task.type === "todo";
+    });
+    callback(todos);
+  });
+}
+
+module.exports = Habitica;
+
 var habit = new Habitica(options.habitica);
-habit.tasks(function(err, resp, body) {
-  if (err) throw err;
-  console.log(resp.statusCode, body);
-});
+habit.todos(function(todos) {
+  console.log(todos);
+})
+//habit.tasks(function(err, resp, body) {
+//  if (err) throw err;
+//  console.log(resp.statusCode, body);
+//});
