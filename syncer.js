73acd94918eb69d43a6b38561cd2244e359bb9da
Henry Baughman <hbaughman741@gmail.com>
2016-01-25 12:34:25 -0500

    Sync txt todos with habitica skeleton

--- syncer.js
+++ syncer.js
@@ -4,13 +4,50 @@ var oldTxtTodos = options.txt.oldTodos
 
 var Dropbox = require('./dropbox');
 var Habitica = require('./habitica');
+var txtDiffer = require("./txtDiffer");
 
 var drop = new Dropbox("/todo/api_test.todo.txt", options.access_token);
 var hab = new Habitica(options.habitica);
 
-var overwritten = [];
+var data = {
+  newTxtTodos: options.txt.newTodos,
+  oldTxtTodos: options.txt.oldTodos,
+  habTodos: options.habitica.todos
+}
+sync(data)
+
 function sync(data) {
-  
+  var updatedTodos = txtDiffer(data.oldTxtTodos, data.newTxtTodos)
+  // TODO: Habitica.createTodo
+  updatedTodos.created.forEach(todo => Habitica.createTodo);
+
+  var overwritten = [];
+
+  // TODO dateCompleted should use the metadata from the last write to dropbox.
+  var dateCompleted = new Date().toISOString();
+  updatedTodos.changed.forEach(todo => {
+    // TODO findHabTodo
+    var habTodo = findHabTodo(todo, data.habTodos)
+    if (todo.isCompleted) {
+      habTodo.dateCompleted = dateCompleted;
+    }
+    else {
+      delete existingHabTodo.dateCompleted;
+    }
+    // TODO Habitica.updateTodo
+    Habitica.updateTodo(habTodo);
+    overwritten.push(habTodo.id);
+  });
+
+  updatedTodos.deleted.forEach(todo => {
+    // TODO findHabTodo
+    var habTodo = findHabTodo(todo, data.habTodos);
+    // TODO Habitica.deleteTodo
+    Habitica.deleteTodo(habTodo);
+    overwritten.push(habTodo.id);
+  });
+
+
 }
 
 function fetchNewTodos(callback) {
@@ -35,5 +72,3 @@ function fetchNewTodos(callback) {
     }
   }
 }
-
-fetchNewTodos(todos => console.log(JSON.stringify(todos, null, 4)))
